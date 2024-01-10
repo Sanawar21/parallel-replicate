a07269ce3545e7279c1ddc88f958b5af0cae5b7a
Sanawar_21 <sanawarsaeed@gmail.com>
2024-01-11 00:16:30 +0500

    fix api urls

--- src/client.py
+++ src/client.py
@@ -11,11 +11,11 @@ SERVER_URL = os.getenv("PRIVATE_SERVER_URL")
 
 
 def download_zip(session_id):
-    url = f"{SERVER_URL}/{session_id}"
+    url = f"{SERVER_URL}/download_zip/{session_id}"
     response = requests.get(url)
     with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref:
         zip_ref.extractall(paths.inputs_folder)
 
     response = requests.delete(
-        f"{SERVER_URL}/{session_id}")
+        f"{SERVER_URL}/delete_files/{session_id}")
     print(response.json())
